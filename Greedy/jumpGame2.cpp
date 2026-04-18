4a2d98b64a36e2af54eeb289df896b62e8e44e26
MrUnknwon-47 <vaibhavsingh8d36@gmail.com>
2026-04-18 23:20:10 +0530

    optimization

--- Greedy/jumpGame2.cpp
+++ Greedy/jumpGame2.cpp
@@ -1,29 +1,33 @@
-int jump(vector<int>& nums) {
-        int jump = 0, l=0, r=0;
-        int n= nums.size();
-        while(r < n){
-            int farest = 0 ;
-            for(int i=l; i<r; i++){
-                farest = max(farest, i + nums[i]);
-            }
-            l=r+1;
-            r=farest;
-            jump++;
+int jump(vector<int> &nums)
+{
+    int jump = 0, l = 0, r = 0;
+    int n = nums.size();
+    while (r < n)
+    {
+        int farest = 0;
+        for (int i = l; i < r; i++)
+        {
+            farest = max(farest, i + nums[i]);
         }
-        
-        return jump;
+        l = r + 1;
+        r = farest;
+        jump++;
     }
 
+    return jump;
+}
 
-
-int jump(vector<int>& nums) {
-4        int jump = 0, currEnd = 0, farest = 0;
-5        for(int i = 0; i < nums.size()-1; i++){
-6            farest = max(farest, i + nums[i]);
-7            if(i == currEnd){
-8                jump++;
-9                currEnd = farest;
-10            }
-11        }
-12        return jump;
-13    }
+int jump(vector<int> &nums)
+{
+    int jump = 0, currEnd = 0, farest = 0;
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        farest = max(farest, i + nums[i]);
+        if (i == currEnd)
+        {
+            jump++;
+            currEnd = farest;
+        }
+    }
+    return jump;
+}
