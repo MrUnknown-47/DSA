2a8e35b26c80b35e66fa9043313711e4195a24b0
MrUnknwon-47 <vaibhavsingh8d36@gmail.com>
2026-04-20 15:12:47 +0530

    Minimum Platforms

---
+++ Greedy/minPlatform.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        int n= arr.size();
+        if(n <= 1) return n;
+        sort(arr.begin(), arr.end());
+        sort(dep.begin(), dep.end());
+        int i=0, j=0;
+        int cnt =0, maxCnt=0;
+        while(i<n){
+            if(arr[i] <= dep[j]){
+                cnt++;
+                i++;
+            }
+            else{
+                cnt--;
+                j++;
+            }
+            maxCnt=max(maxCnt, cnt);
+        }
+        return maxCnt;
+    }
+};
