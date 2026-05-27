c8e1d6f1225bc95925d4ad2a2241204f16bfea1b
MrUnknwon-47 <vaibhavsingh8d36@gmail.com>
2026-05-28 00:35:19 +0530

    Cycle in Undirected Graph DFS

---
+++ Graph/cycleInUndirDFS.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int> *l;
+
+public:
+    Graph(int V){
+        this->V = V;
+        l = new list<int>[V];
+        // arr = new int[V];
+    }
+
+    void addEdge(int u, int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    bool isCycleUndireDFS(int src, int par, vector<bool> &vis){
+        vis[src] = true;
+        list<int> neighbours = l[src];
+        for(int v: neighbours){
+            if(!vis[v]){
+                if(isCycleUndireDFS(v, src, vis)) return true;
+            }
+            else if(v != par) return true;
+        }
+        return false;
+    }
+
+    bool isCycle(){
+        vector<bool> vis(V, false);
+
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(isCycleUndireDFS(i, -1, vis)) return true;
+            }
+        }
+        return false;
+    }
+};
+int main(){
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+    cout<< g.isCycle() << endl;
+    return 0;
+}
