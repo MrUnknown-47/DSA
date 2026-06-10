9290016fea1c4a97c335478cce702ea3c112b99d
MrUnknown-47 <vaibhavsingh8d36@gmail.com>
2026-06-11 01:19:07 +0530

    Disjoint Set Union

--- Graph/DisjointSetUnion.cpp
+++ Graph/DisjointSetUnion.cpp
@@ -2,66 +2,75 @@
 #include <vector>
 using namespace std;
 
-class DisjointSetUnion{
+class DisjointSetUnion
+{
 public:
     int n;
     vector<int> par, rank;
 
-    DisjointSetUnion(int n){
+    DisjointSetUnion(int n)
+    {
         this->n = n;
-        
-        for(int i=0; i<n; i++){
+
+        for (int i = 0; i < n; i++)
+        {
             par.push_back(i);
             rank.push_back(0);
         }
-
     }
 
-    void unionByRank(int a, int b){
+    void unionByRank(int a, int b)
+    {
         int parA = find(a);
         int parB = find(b);
 
-        if(parA == parB) return;
+        if (parA == parB)
+            return;
 
-        if(rank[parA] == rank[parB]){
+        if (rank[parA] == rank[parB])
+        {
             par[parB] = parA;
             rank[parA]++;
         }
-        else if(rank[parA] > rank[parB])
+        else if (rank[parA] > rank[parB])
             par[parB] = parA;
         else
             par[parA] = parB;
     }
 
-    int find(int x){
-        if(par[x] == x) return x;
+    int find(int x)
+    {
+        if (par[x] == x)
+            return x;
 
         return par[x] = find(par[x]);
     }
 
-    void getInfo(){
-        for(int i=0; i<n; i++)
+    void getInfo()
+    {
+        for (int i = 0; i < n; i++)
             cout << par[i] << " ";
         cout << endl;
-        
-        for(int i=0; i<n; i++)
+
+        for (int i = 0; i < n; i++)
             cout << rank[i] << " ";
         cout << endl;
     }
 };
 
-int main (){
+int main()
+{
     DisjointSetUnion dsu(6);
 
-    dsu. unionByRank (0,2);
-    cout << dsu.find (2) <<endl;
+    dsu.unionByRank(0, 2);
+    cout << dsu.find(2) << endl;
 
-    dsu.unionByRank (1,3);
-    dsu.unionByRank(2,5) ;
-    dsu.unionByRank (0,3) ;
-    cout << dsu.find (2)<<endl;
+    dsu.unionByRank(1, 3);
+    dsu.unionByRank(2, 5);
+    dsu.unionByRank(0, 3);
+    cout << dsu.find(2) << endl;
 
-    dsu.unionByRank(0,4);
+    dsu.unionByRank(0, 4);
 
     dsu.getInfo();
 
