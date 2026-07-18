111b78a5e3b1007926f6c5c181448b64abd9803b
MrUnknown-47 <vaibhavsingh8d36@gmail.com>
2026-07-18 14:57:01 +0530

    70. Climbing Stairs

---
+++ DP/climbingStairs.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1 || n == 2)
+            return n;
+
+        int prev1 = 1, prev2 = 2;
+        int result = prev1;
+
+        for (int i = 3; i <= n; i++)
+        {
+            result = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = result;
+        }
+
+        return result;
+    }
+};
--- DP/fib.cpp
+++ DP/fib.cpp
@@ -1,34 +1,41 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int fib(int n) {
-    if(n <= 1) return n;
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
 
-    return fib(n-1) + fib(n-2);
+    return fib(n - 1) + fib(n - 2);
 }
 
-int fibDP(int n, vector<int> &f) {
-    if(n <= 1) return n;
+int fibDP(int n, vector<int> &f)
+{
+    if (n <= 1)
+        return n;
 
-    if(f[n] != -1) return f[n];
+    if (f[n] != -1)
+        return f[n];
 
-    return f[n] = fibDP(n-1, f) + fibDP(n-2, f);
+    return f[n] = fibDP(n - 1, f) + fibDP(n - 2, f);
 }
 
-int fibTabDP(int n){
-    vector<int> dp(n+1);
+int fibTabDP(int n)
+{
+    vector<int> dp(n + 1);
     dp[0] = 0;
     dp[1] = 1;
 
-    for(int i=2; i<=n; i++)
-        dp[i] = dp[i-1] + dp[i-2];
-    
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
     return dp[n];
 }
 
-int main(){
+int main()
+{
     int n = 6;
-    vector<int> f(n+1, -1); 
+    vector<int> f(n + 1, -1);
     cout << fibTabDP(n) << endl;
 }
